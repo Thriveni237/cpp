00ab32b26682899c5f88e2bee4883074781245e3
thriveni chowdary <thrivenidamancharla@gmail.com>
2025-11-20 22:08:42 +0530

    Add inheritance.cpp

---
+++ inheritance.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+class Animal { public:
+    void speak() { cout << "Animal speaks" << endl; } };
+class Dog : public Animal { public:
+    void speak() { cout << "Dog barks" << endl; } };
+int main() {
+    Dog d; d.speak(); return 0;
+}
