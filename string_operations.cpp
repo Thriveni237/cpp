d76f1a4ca776eeb16d40a514bd0ba9bdf984140e
thriveni chowdary <thrivenidamancharla@gmail.com>
2025-11-20 22:03:29 +0530

    Add string_operations.cpp

---
+++ string_operations.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str1 = "Hello";
+    string str2 = "World";
+    string result = str1 + " " + str2;
+    cout << result << endl;
+    cout << "Length: " << result.length() << endl;
+    return 0;
+}
